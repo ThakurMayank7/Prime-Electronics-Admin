5f21296b396c616366dc10b64999d1b0e31877bc
Mayank <mayanksingh8181335335@gmail.com>
2024-12-08 05:51:06 +0530

    made add item template

--- app/items/add/page.tsx
+++ app/items/add/page.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+'use client';
 
 function ItemsAdd() {
+
+const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+
+    e.preventDefault();
+}
+
+
   return (
-    <div>ItemsAdd</div>
+    <div className="flex items-center justify-center h-screen">
+        <form className="flex flex-col gap-2 border-2 border-black p-4" onSubmit={handleSubmit}>
+<h1 className="text-center text-4xl font-bold p-2">Item Details</h1>
+<div className="flex flex-row items-center justify-center">
+<span className="font-semibold mr-2 w-1/2 text-center">Title
+</span>
+<input className="bg-gray-200 border-2 border-black rounded p-2" type="text" />
+</div>
+<div className="flex flex-row items-center justify-center">
+<span className="font-semibold mr-2 w-1/2 text-center">Description
+</span>
+<input className="bg-gray-200 border-2 border-black rounded p-2" type="text" />
+</div>
+<div className="flex flex-row items-center justify-center">
+<span className="font-semibold mr-2 w-1/2 text-center">Category
+</span>
+<input className="bg-gray-200 border-2 border-black rounded p-2" type="text" />
+</div>
+<div className="flex flex-row items-center justify-center">
+<span className="font-semibold mr-2 w-1/2 text-center">Brand
+</span>
+<input className="bg-gray-200 border-2 border-black rounded p-2" type="text" />
+</div>
+
+
+<br />
+            <button className="bg-green-500 hover:bg-green-600 rounded p-2 border-2 border-black font-bold" type="submit">Add Item</button>
+        </form>
+    </div>
   )
 }
 
