b7dced7c0b9b6e07be0acaabf1fc0dc4e10d2332
Mayank <mayanksingh8181335335@gmail.com>
2024-12-14 06:14:12 +0530

    item creation working few bugs need to be fixed for backend database

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,60 +1,80 @@
-'use server';
+"use server";
 
 import { adminDb } from "@/firebase/admin";
 
-export async function addItem(title:string,description:string,category:string,brand:string)
-{
-    await adminDb.collection("test").add(
-        {
-            "title":title,
-            "description":description,
-            "category":category,
-            "brand":brand
-        });
+export async function addItem(
+  title: string,
+  description: string,
+  category: string,
+  brand: string
+) {
+  await adminDb.collection("test").add({
+    title: title,
+    description: description,
+    category: category,
+    brand: brand,
+  });
 }
 
 export async function verifyAdmin(email: string): Promise<boolean> {
-    try {
-      const snapshot = await adminDb.collection('admins').where('email', '==', email).get(); // Query and get the data
-  
-      // Check if any document was found
-      if (snapshot.empty) {
-        return false; // No admin found with the given email
-      } else {
-        return true; // Admin exists
-      }
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return false; // Handle error and return false
+  try {
+    const snapshot = await adminDb
+      .collection("admins")
+      .where("email", "==", email)
+      .get(); // Query and get the data
+
+    // Check if any document was found
+    if (snapshot.empty) {
+      return false; // No admin found with the given email
+    } else {
+      return true; // Admin exists
     }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return false; // Handle error and return false
   }
+}
 
-  export async function createBrand(brandName: string,brandDescription: string,logoRef:string): Promise<boolean> {
-  
-  try{
-
-    const result=await adminDb.collection('brands').add(
-      {
-        brandName: brandName,
-        brandDescription:brandDescription,
-        logoRef:logoRef
-      }
-    );
-      if(!result.id)
-      {
-        return false;
-      }
-      return true;
-
-
-
-  }
-  catch(error)
-  {
-    console.error('error');
+export async function createBrand(
+  brandName: string,
+  brandDescription: string,
+  logoRef: string
+): Promise<boolean> {
+  try {
+    const result = await adminDb.collection("brands").add({
+      brandName: brandName,
+      brandDescription: brandDescription,
+      logoRef: logoRef,
+    });
+    if (!result.id) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    console.error(error);
     return false;
   }
-  
-  
+}
+export async function createItem(
+  {itemName,itemDescription,displayImageRef,imagesRefs}:
+  {itemName: string,
+  itemDescription: string,
+  displayImageRef: string,
+  imagesRefs:string[]}
+): Promise<boolean> {
+  try {
+    const result = await adminDb.collection("items").add({
+      itemName:itemName,
+      itemDescription:itemDescription,
+      displayImageRef:displayImageRef,
+      imagesRefs:imagesRefs
+    });
+    if (!result.id) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
   }
-  
+}
--- app/items/add/page.tsx
+++ app/items/add/page.tsx
@@ -7,34 +7,31 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { format } from "date-fns";
+import { createItem } from "@/actions/actions";
 
 function ItemsAdd() {
+  const { user, loading } = useAuth();
 
-    const { user, loading } = useAuth();
-  
-    const router = useRouter();
+  const router = useRouter();
   const [title, setTitle] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [category, setCategory] = useState<string>("");
   const [brand, setBrand] = useState<string>("");
 
-
   const [file, setFile] = useState<File | null>(null);
-  
-    const [uploading, setUploading] = useState(false);
-  
-    const [publicId, setPublicId] = useState<string | null>(null);
-    const [error, setError] = useState<string | null>(null);
-  
-    const [finalId, setFinalId] = useState<string | null>(null);
-    const [creating, setCreating] = useState(false);
 
-    const [images,setImages] = useState<File[]>([]);
-    const [imagesId,setImagesId] = useState<string[]>([]);
-    const [uploadingImages,setUploadingImages] = useState<boolean>(false);
-  
+  const [uploading, setUploading] = useState(false);
 
+  const [publicId, setPublicId] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
+  const [finalId, setFinalId] = useState<string>("");
+  const [creating, setCreating] = useState(false);
+  const [finalImagesId, setFinalImagesId] = useState<string[]>([]);
+
+  const [images, setImages] = useState<File[]>([]);
+  const [imagesId, setImagesId] = useState<string[]>([]);
+  const [uploadingImages, setUploadingImages] = useState<boolean>(false);
 
   useEffect(() => {
     if (user === null && loading === false) {
@@ -43,88 +40,109 @@ function ItemsAdd() {
   }, [user, router, loading]);
 
   if (loading) {
-    return <div className="h-screen w-screen flex items-center justify-center"><Spinner/></div>;
+    return (
+      <div className="h-screen w-screen flex items-center justify-center">
+        <Spinner />
+      </div>
+    );
   }
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-
-
-    if(file && images &&title && description&&category &&brand)
-    {
-      setCreating(true)
+    if (file && images && title && description && category && brand) {
+      setCreating(true);
       const now = new Date();
-      try{
-// Uploading Display Image
+      try {
+        // Uploading Display Image
 
-const formDataDisplay = new FormData();
+        const formDataDisplay = new FormData();
         formDataDisplay.append("file", file);
-        formDataDisplay.append("folder", "primeElectronics/draft");
-  
+        formDataDisplay.append(
+          "folder",
+          "primeElectronics/items/displayImages"
+        );
+
         const response = await fetch("/api/add-image", {
           method: "POST",
           body: formDataDisplay,
         });
-  
+
         if (!response.ok) {
           const errorData = await response.json();
           setError(errorData.error || "Upload failed");
           return;
         }
-  
+
         const data = await response.json();
         setFinalId(data.publicId);
-        
+
         // Uploading other Images
-        
-        for(let i=0;i<images.length;i++) {
-
-
+        setFinalImagesId([]);
+        for (let i = 0; i < images.length; i++) {
           const formData = new FormData();
-        formData.append("file", file);
-        formData.append("folder", "primeElectronics/draft");
-  
-        const response = await fetch("/api/add-image", {
-          method: "POST",
-          body: formData,
+          formData.append("file", images[i]);
+          formData.append("folder", "primeElectronics/items");
+
+          const response = await fetch("/api/add-image", {
+            method: "POST",
+            body: formData,
+          });
+
+          if (!response.ok) {
+            const errorData = await response.json();
+            setError(errorData.error || "Upload failed");
+            return;
+          }
+
+          const data = await response.json();
+
+          setFinalImagesId((prev) => [...prev, data.publicId]);
+        }
+
+        // Adding to firebase
+
+        const result = await createItem({
+          itemName: title,
+          itemDescription: description,
+          displayImageRef: finalId,
+          imagesRefs: finalImagesId,
         });
-  
-        if (!response.ok) {
-          const errorData = await response.json();
-          setError(errorData.error || "Upload failed");
-          return;
+
+        if (result) {
+          toast("Created a new Item", {
+            description:
+              title +
+              " : " +
+              ". At " +
+              format(now.toISOString(), "yyyy-MM-dd HH:mm:ss"),
+            action: null,
+          });
+
+          router.refresh();
+        } else {
+          toast("Error Occurred", {
+            description:
+              "Could not Create this Item : " +
+              error +
+              ". At " +
+              format(now.toISOString(), "yyyy-MM-dd HH:mm:ss"),
+            action: null,
+          });
         }
-  
-        const data = await response.json();
-        setFinalId(data.publicId);
-
-
-
-
-        }
-
-
-
-
-
-      }
-      catch(error)
-      {
+      } catch (error) {
         toast("Error Occurred", {
-          description: "Could not Create this Item : "+error+". At " + format(now.toISOString(), "yyyy-MM-dd HH:mm:ss"),
+          description:
+            "Could not Create this Item : " +
+            error +
+            ". At " +
+            format(now.toISOString(), "yyyy-MM-dd HH:mm:ss"),
           action: null,
         });
+      } finally {
+        setCreating(false);
       }
-      finally{
-setCreating(false)
-      }
-
     }
-
-    // TODO add item here
-
-
   };
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -135,78 +153,74 @@ setCreating(false)
   };
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      if (e.target.files && e.target.files[0]) {
-        setFile(e.target.files[0]);
-      }
-    };
-  
-    const handleUpload = async () => {
-      if (!file) {
-        setError("Please select a file first.");
+    if (e.target.files && e.target.files[0]) {
+      setFile(e.target.files[0]);
+    }
+  };
+
+  const handleUpload = async () => {
+    if (!file) {
+      setError("Please select a file first.");
+      return;
+    }
+
+    setUploading(true);
+    setError(null);
+    setPublicId(null);
+
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("folder", "primeElectronics/draft");
+
+      const response = await fetch("/api/add-image", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        setError(errorData.error || "Upload failed");
         return;
       }
-  
-      setUploading(true);
-      setError(null);
-      setPublicId(null);
-  
+
+      const data = await response.json();
+      setPublicId(data.publicId);
+    } catch (err) {
+      console.log(err);
+      setError("An unexpected error occurred.");
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  const handleImagesUpload = async () => {
+    if (images.length === 0 || images.length === imagesId.length) {
+      return;
+    }
+    setUploadingImages(true);
+    setImagesId([]);
+
+    for (let i = 0; i < images.length; i++) {
       try {
-        const formData = new FormData();
-        formData.append("file", file);
-        formData.append("folder", "primeElectronics/draft");
-  
-        const response = await fetch("/api/add-image", {
-          method: "POST",
-          body: formData,
-        });
-  
-        if (!response.ok) {
-          const errorData = await response.json();
-          setError(errorData.error || "Upload failed");
-          return;
-        }
-  
-        const data = await response.json();
-        setPublicId(data.publicId);
-      } catch (err) {
-        console.log(err);
-        setError("An unexpected error occurred.");
-      } finally {
-        setUploading(false);
-      }
-    };
-
-
-
-    const handleImagesUpload=async()=>{
-      if (images.length===0 || images.length===imagesId.length) {
-        return;
-      }
-      setUploadingImages(true);
-      setImagesId([]);
-
-      for(let i=0;i<images.length;i++) {
-
-        
-        try {
         const formData = new FormData();
         formData.append("file", images[i]);
         formData.append("folder", "primeElectronics/draft");
-  
+
         const response = await fetch("/api/add-image", {
           method: "POST",
           body: formData,
         });
-  
+
         if (!response.ok) {
           const errorData = await response.json();
           setError(errorData.error || "Upload failed");
           return;
         }
-  
+
         const data = await response.json();
-        
-        setImagesId((prev)=>[...prev,data.publicId])
+
+        setImagesId((prev) => [...prev, data.publicId]);
       } catch (err) {
         console.log(err);
         setError("An unexpected error occurred.");
@@ -214,158 +228,169 @@ setCreating(false)
         setUploadingImages(false);
       }
     }
-    };
-    
+  };
 
   return (
-
     <div>
-
-      {creating?
-      <div className="flex items-center flex-col my-16">
-      <span className="text-4xl font-semibold">Creating the item! Please wait...</span>
-      <Spinner/>
-      </div>
-
-      :
-
-
+      {creating ? (
+        <div className="flex items-center flex-col my-16">
+          <span className="text-4xl font-semibold">
+            Creating the item! Please wait...
+          </span>
+          <Spinner />
+        </div>
+      ) : (
         <div className="flex flex-col gap-2">
-
-    <div className="flex items-center justify-center h-full gap-2">
-      <form
-        className="flex flex-col gap-2 border-2 border-black p-4"
-        onSubmit={handleSubmit}
-      >
-        <h1 className="text-center text-4xl font-bold p-2">Item Details</h1>
-        <div className="flex flex-row items-center justify-center">
-          <span className="font-semibold mr-2 w-1/2 text-center">Title</span>
-          <input
-            className="bg-gray-200 border-2 border-black rounded p-2"
-            type="text" value={title} onChange={(e)=>setTitle(e.target.value)}
-          />
-        </div>
-        <div className="flex flex-row items-center justify-center">
-          <span className="font-semibold mr-2 w-1/2 text-center">
-            Description
-          </span>
-          <input
-            className="bg-gray-200 border-2 border-black rounded p-2"
-            type="text" value={description} onChange={(e)=>setDescription(e.target.value)}
-          />
-        </div>
-        <div className="flex flex-row items-center justify-center">
-          <span className="font-semibold mr-2 w-1/2 text-center">Category</span>
-          <input
-            className="bg-gray-200 border-2 border-black rounded p-2"
-            type="text" value={category} onChange={(e)=>setCategory(e.target.value)}
-          />
-        </div>
-        <div className="flex flex-row items-center justify-center">
-          <span className="font-semibold mr-2 w-1/2 text-center">Brand</span>
-          <input
-            className="bg-gray-200 border-2 border-black rounded p-2"
-            type="text" value={brand} onChange={(e)=>setBrand(e.target.value)}
-          />
-        </div>
-
-        <br />
-        <button
-          className="bg-green-500 hover:bg-green-600 rounded p-2 border-2 border-black font-bold"
-          type="submit"
-        >
-          Add Item
-        </button>
-      </form>
-      <div className="border-2 border-black p-4">
-        <div className="flex justify-center">
-          <span className="text-2xl font-semibold">
-            {publicId ? "Preview of Display Image" : "Select Display Image"}
-          </span>
-        </div>
-
-        {!publicId && (
-          <div>
-            <input
-              type="file"
-              accept="image/*"
-              multiple={false} // Disallow multiple file selection
-              onChange={handleFileChange}
-              className="mb-4"
-            />
-            <button
-              onClick={handleUpload}
-              disabled={uploading}
-              className="bg-teal-200 p-2 rounded hover:bg-teal-500"
+          <div className="flex items-center justify-center h-full gap-2">
+            <form
+              className="flex flex-col gap-2 border-2 border-black p-4"
+              onSubmit={handleSubmit}
             >
-              {uploading ? "Uploading..." : "Upload"}
-            </button>
-            {error && <p className="text-red-500 mt-4">{error}</p>}
-          </div>
-        )}
-        {publicId && (
-          <div className="bg-cyan-400">
-            <CldImage
-              width="400"
-              height="400"
-              quality="auto"
-              crop="auto"
-              src={publicId}
-              alt="Description of my image"
-            />
-          </div>
-        )}
-      </div>
-    </div>
+              <h1 className="text-center text-4xl font-bold p-2">
+                Item Details
+              </h1>
+              <div className="flex flex-row items-center justify-center">
+                <span className="font-semibold mr-2 w-1/2 text-center">
+                  Title
+                </span>
+                <input
+                  className="bg-gray-200 border-2 border-black rounded p-2"
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-row items-center justify-center">
+                <span className="font-semibold mr-2 w-1/2 text-center">
+                  Description
+                </span>
+                <input
+                  className="bg-gray-200 border-2 border-black rounded p-2"
+                  type="text"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-row items-center justify-center">
+                <span className="font-semibold mr-2 w-1/2 text-center">
+                  Category
+                </span>
+                <input
+                  className="bg-gray-200 border-2 border-black rounded p-2"
+                  type="text"
+                  value={category}
+                  onChange={(e) => setCategory(e.target.value)}
+                />
+              </div>
+              <div className="flex flex-row items-center justify-center">
+                <span className="font-semibold mr-2 w-1/2 text-center">
+                  Brand
+                </span>
+                <input
+                  className="bg-gray-200 border-2 border-black rounded p-2"
+                  type="text"
+                  value={brand}
+                  onChange={(e) => setBrand(e.target.value)}
+                />
+              </div>
 
-    <div className="flex items-center justify-center">
-    
-    <div className="border-2 border-black rounded">
-
-
-    <input
-        type="file"
-        accept="image/*"
-        multiple
-        onChange={handleImageChange}
-      />
-
-      <span>{images.length}</span>
-
-      <button
-              onClick={handleImagesUpload}
-              className={`p-2 rounded ${!uploadingImages?'bg-teal-200 hover:bg-teal-500':"bg-teal-700"}`}
-              disabled={uploadingImages}
-            >
-              {uploadingImages?"Uploading":"Upload Selected Images"}
-            </button>
-
-            {imagesId && (
-              <>
               <br />
-  <div className="grid grid-cols-3 gap-4">
-    {imagesId.map((imageId) => (
-      <div key={imageId}>
-        <CldImage
-          width="300"
-          height="300"
-          quality="auto"
-          crop="auto"
-          src={imageId}
-          alt="Description of my image"
-          />
-      </div>
-    ))}
-  </div>
-    </>
-)}
-    </div>
-      </div>
-    
-    
-    
-    </div>
-  }
+              <button
+                className="bg-green-500 hover:bg-green-600 rounded p-2 border-2 border-black font-bold"
+                type="submit"
+              >
+                Add Item
+              </button>
+            </form>
+            <div className="border-2 border-black p-4">
+              <div className="flex justify-center">
+                <span className="text-2xl font-semibold">
+                  {publicId
+                    ? "Preview of Display Image"
+                    : "Select Display Image"}
+                </span>
+              </div>
+
+              {!publicId && (
+                <div>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    multiple={false} // Disallow multiple file selection
+                    onChange={handleFileChange}
+                    className="mb-4"
+                  />
+                  <button
+                    onClick={handleUpload}
+                    disabled={uploading}
+                    className="bg-teal-200 p-2 rounded hover:bg-teal-500"
+                  >
+                    {uploading ? "Uploading..." : "Upload"}
+                  </button>
+                  {error && <p className="text-red-500 mt-4">{error}</p>}
+                </div>
+              )}
+              {publicId && (
+                <div className="bg-cyan-400">
+                  <CldImage
+                    width="400"
+                    height="400"
+                    quality="auto"
+                    crop="auto"
+                    src={publicId}
+                    alt="Description of my image"
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center">
+            <div className="border-2 border-black rounded">
+              <input
+                type="file"
+                accept="image/*"
+                multiple
+                onChange={handleImageChange}
+              />
+
+              <span>{images.length}</span>
+
+              <button
+                onClick={handleImagesUpload}
+                className={`p-2 rounded ${
+                  !uploadingImages
+                    ? "bg-teal-200 hover:bg-teal-500"
+                    : "bg-teal-700"
+                }`}
+                disabled={uploadingImages}
+              >
+                {uploadingImages ? "Uploading" : "Upload Selected Images"}
+              </button>
+
+              {imagesId && (
+                <>
+                  <br />
+                  <div className="grid grid-cols-3 gap-4">
+                    {imagesId.map((imageId) => (
+                      <div key={imageId}>
+                        <CldImage
+                          width="300"
+                          height="300"
+                          quality="auto"
+                          crop="auto"
+                          src={imageId}
+                          alt="Description of my image"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
